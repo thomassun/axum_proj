3134cad19e751a17c752cca04517724174a6665b
Thomas Sun <sguoqing@gmail.com>
2024-01-02 13:11:18 +0800

    axum+db

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+pub struct Point(i32, i32);
+
+impl From<(i32, i32)> for Point {
+    fn from(value: (i32, i32)) -> Self {
+        Point(value.0, value.1)
+    }
+}
+impl From<i32> for Point {
+    fn from(v: i32) -> Self {
+        Point(v, v)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_name() {
+        let p: Point = (32, 4).into();
+        assert_eq!(p.1, 4);
+        let p: Point = 32.into();
+        assert_eq!(p.1, 32);
+        let x = i64::from(3i32);
+    }
+}
