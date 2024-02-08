57e9b09a61a10786f65e8bda0604810ce024acec
thomas.sun <sguoqing@gmail.com>
2024-02-08 16:45:05 +0800

    moduler

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use axum::{routing::get, Router};
+use axum::{
+    extract::Path,
+    routing::get,
+    Router,
+};
 use axum_example::route::user::user_route;
+use tracing::info;
 
 #[tokio::main]
 async fn main() {
@@ -9,32 +14,35 @@ async fn main() {
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/", get(root))
+        .route("/:p/:p", get(root))
         .merge(user_route());
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("Listening on 0.0.0.0:3000....");
+    info!("Listening on 0.0.0.0:3000....");
     axum::serve(listener, app).await.unwrap();
 }
 
 // basic handler that responds with a static string
-async fn root() -> &'static str {
+async fn root(Path(pair): Path<(u32,i32)>) -> &'static str {
+    println!("{:?}", pair);
     "Hello, World!"
+    // pair
 }
 #[cfg(test)]
 mod tests {
+
     #[test]
     fn test_name() {
-        let v = [Err("error!"), Ok(1), Ok(2), Ok(3), Err("foo")];
-        let res: Result<i32, &str> = v.into_iter().sum();
-        assert_eq!(res, Err("error!"));
-        let v = [Ok(1), Ok(2), Ok(21)];
-        let res: Result<i32, &str> = v.into_iter().product();
-        assert_eq!(res, Ok(42));
-        let mut v = vec![1, 2, 3];
-        let elem = v.iter_mut().next().unwrap();
-        *elem = 12;
-        assert_eq!(v, vec![12, 2, 3]);
+        let mut v = [Err("error!"), Ok(1), Ok(2), Ok(3), Err("foo")];
+        let w = [Ok('c'), Err("error!"), Ok('2'), Ok('3'), Err("foo")];
+        for v_i in &mut v {
+            *v_i = Ok(111) as Result<i32, &str>;
+            // println!("{v_i:?}");
+        }
+        // let z = v;
+        println!("{w:?}");
+        println!("{v:?}");
+        assert_eq!(Some(1).and(Some(2)), Some(2));
     }
 }
